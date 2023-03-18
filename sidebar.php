810ba191e5fad790ec86bdc983461b0821bd197f
Frank Schroeder <frank@datateam.lu>
2023-03-18 17:09:27 +0100

    small cosmetic changes sidebar added defiant

--- sidebar.php
+++ sidebar.php
@@ -16,6 +16,8 @@ if ( ! is_active_sidebar( 'default-sidebar' ) ) {
 ?>
 
 <aside id="secondary" class="widget-area" aria-label="<?php esc_attr_e( 'Sidebar', 'lcars' ); ?>">
-    
 	<?php dynamic_sidebar( 'default-sidebar' ); ?>
+	<br/><br/><br/>
+	<p align="center">USS Defiant - NX-74205</p>
+    <img class="img_center" src="<?php echo get_template_directory_uri(); ?>/images/defiant.png" border="0" alt="Defiant" width="160" height="221" style="vertical-align: middle;" />
 </aside><!-- #secondary -->
