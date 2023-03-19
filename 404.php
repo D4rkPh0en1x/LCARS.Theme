22249cca0f91dc0e2f40e161a61ec90292aa40ae
Frank Schroeder <frank@datateam.lu>
2023-03-19 16:22:27 +0100

    Modifications for featured images etc

--- 404.php
+++ 404.php
@@ -6,21 +6,30 @@
 
 get_header();
 ?>
-<div id="primary" class="content-area">
-    <main id="main" class="site-main">
-        <section class="error-404 not-found">
-            <header class="page-header">
-                <h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'lcars' ); ?></h1>
-            </header><!-- .page-header -->
+<div class="container">
+    <div class="row">
+        <div class="col-lg-9">
+            <div id="primary" class="content-area">
+                <main id="main" class="site-main">
+                    <section class="error-404 not-found">
+                        <header class="page-header">
+                            <h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'lcars' ); ?></h1>
+                        </header><!-- .page-header -->
 
-            <div class="page-content">
-                <p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'ninestars' ); ?></p>
+                        <div class="page-content">
+                            <p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'ninestars' ); ?></p>
 
-                <?php get_search_form(); ?>
-            </div><!-- .page-content -->
-        </section><!-- .error-404 -->
+                            <?php get_search_form(); ?>
+                        </div><!-- .page-content -->
+                    </section><!-- .error-404 -->
 
-    </main><!-- #main -->
-</div><!-- #primary -->
+                </main><!-- #main -->
+            </div><!-- #primary -->
+        </div>
+        <div class="col-lg-3">
+            <?php get_sidebar(); ?>
+        </div>
+    </div><!-- .row -->
+</div><!-- .container -->            
 <?php
 get_footer();
--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -16,8 +16,26 @@
     </header>
     <!-- Post thumbnail -->
     <?php
+    $post_id=get_the_ID();
+    $featured_img_url = get_the_post_thumbnail_url( $post_id );
+    
     if ( has_post_thumbnail() ) :
-        the_post_thumbnail( 'large' ); // full, large, medium, custom size
+        
+        echo '<a class="lcars_featured_image glightbox" href="' . $featured_img_url . '"   
+        data-type="image" 
+        data-effect="fade" 
+        data-width="900px" 
+        data-height="auto" 
+        data-zoomable="false" 
+        data-draggable="false" 
+        data-lightbox="' . $post_id . '" 
+        data-title="' . get_the_title() . '">';
+
+
+        the_post_thumbnail( array(250, 250) ); // full, large, medium, custom size
+     
+        echo '</a><br/>';
+
     endif;
     ?>
     <!-- Post Content -->
